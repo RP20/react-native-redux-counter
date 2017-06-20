ae4ef38f84ba434ca1fbd9ad5fcb3e03c7bd082f
Riken Patel <rikenpatel@Rikens-MacBook-Pro.local>
2017-06-20 12:30:21 -0700

    Initial Commit

---
+++ app/components/Main.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import {connect} from 'react-redux';
+import {incrementCounter, decrementCounter} from '../actions';
+
+var Main = React.createClass ({
+
+  incrementCounter() {
+    this.props.dispatch(incrementCounter);
+  },
+
+  decrementCounter() {
+    this.props.dispatch(decrementCounter);
+  },
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.countText}>
+          {this.props.count}
+        </Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={this.incrementCounter}>
+            <Text style={styles.countText}>+</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.decrementCounter}>
+            <Text style={styles.countText}>-</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  countText: {
+    fontSize: 50,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
+});
+
+var mapStateToProps = (state) => {
+  return {
+    count: state.count
+  }
+}
+
+module.exports = connect(mapStateToProps)(Main);
---
+++ app/reducer/index.js
@@ -0,0 +1,15 @@
+module.exports = (state={}, action) => {
+  switch (action.type) {
+    case 'INCREMENT_COUNTER':
+      return {
+        count: state.count+1
+      }
+    case 'DECREMENT_COUNTER':
+        return {
+          count: state.count-1
+        }
+        
+    default:
+      return state;
+  }
+}
